be4c76ef19a00fa8c610583fbcc55e448181d71d
Prayag Sangode <prayag.rhce@gmail.com>
2024-12-27 16:11:29 +0000

    update

--- provider.tf
+++ provider.tf
@@ -11,3 +11,20 @@ terraform {
     dynamodb_table = "dynamodb-state-locking"
   }
 }
+
+# Fetch the EKS Cluster details
+data "aws_eks_cluster" "my_cluster" {
+  name = aws_eks_cluster.my_cluster.name
+}
+
+# Fetch the EKS Cluster authentication details
+data "aws_eks_cluster_auth" "my_cluster" {
+  name = aws_eks_cluster.my_cluster.name
+}
+
+# Kubernetes provider configuration to authenticate with the EKS cluster
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.my_cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.my_cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.my_cluster.token
+}
