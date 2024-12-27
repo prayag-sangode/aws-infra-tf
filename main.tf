68926ab5502a0dada36f63b91374731a5756e07b
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2024-12-27 15:59:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -125,6 +125,29 @@ resource "aws_security_group" "bastion_sg" {
   }
 }
 
+# EKS Node Security Group
+resource "aws_security_group" "eks_node_sg" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "eks-node-sg"
+  }
+}
+
 # Bastion Host Instance
 resource "aws_instance" "bastion" {
   ami                         = "ami-005fc0f236362e99f" # Ubuntu22 AMI
@@ -208,7 +231,7 @@ resource "aws_eks_cluster" "my_cluster" {
 
   vpc_config {
     subnet_ids         = [aws_subnet.private_az1.id, aws_subnet.private_az2.id]
-    security_group_ids = [aws_security_group.bastion_sg.id]  # Ensure this is properly referenced
+    security_group_ids = [aws_security_group.eks_node_sg.id]
   }
 }
 
@@ -218,6 +241,7 @@ resource "aws_eks_node_group" "my_node_group" {
   node_group_name = "my-node-group"
   node_role_arn   = aws_iam_role.eks_node.arn
   subnet_ids      = [aws_subnet.private_az1.id, aws_subnet.private_az2.id]
+  security_groups = [aws_security_group.eks_node_sg.id]
 
   scaling_config {
     desired_size = 1
